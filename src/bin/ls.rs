68113a1fe93dd4cf4cd195daced2a8225db20882
Eric Lavigne <hi_eric@hotmail.com>
2024-10-08 20:18:05 -0400

    Clean up ls command

--- src/bin/ls.rs
+++ src/bin/ls.rs
@@ -1,20 +1,23 @@
-use std::fs::DirEntry;
+use std::cell::LazyCell;
+use std::fmt::Display;
+use std::fs::Metadata;
+use std::os::unix::ffi::OsStrExt;
 use std::os::unix::fs::MetadataExt;
+use std::path::{Path, PathBuf};
 use std::time::UNIX_EPOCH;
-use std::{
-    fs,
-    path::{Path, PathBuf},
-};
 
 use clap::Parser;
 use time::macros::format_description;
 use time::UtcOffset;
 use time::{parsing::Parsed, OffsetDateTime};
 
+const UTC_OFFSET: LazyCell<UtcOffset> =
+    LazyCell::new(|| UtcOffset::current_local_offset().unwrap());
+
 #[derive(Parser)]
 #[command(version, about = "list directory contents", long_about = None)]
 struct Cli {
-    path: Option<PathBuf>,
+    path: Option<Vec<PathBuf>>,
 
     /// do not ignore entries starting with .
     #[arg(short, long)]
@@ -25,73 +28,144 @@ struct Cli {
     long: bool,
 }
 
-fn main() {
-    let cli = Cli::parse();
+struct LSFile<'a> {
+    path: PathBuf,
+    cli: &'a Cli,
+    metadata: Option<Metadata>,
+}
 
-    let path = if let Some(path) = cli.path.as_deref() {
-        path
-    } else {
-        &Path::new(".")
-    };
-
-    let path_metadata = fs::metadata(path);
-    let path_metadata = match path_metadata {
-        Err(err) => panic!("{}", err),
-        Ok(metadata) => metadata,
-    };
-    if path_metadata.is_file() {
-        println!("{}", path.to_path_buf().display());
-    } else if path_metadata.is_dir() {
-        let mut paths = path
-            .read_dir()
-            .expect("Could not read dir")
-            .filter_map(|entry| entry.ok())
-            .collect::<Vec<DirEntry>>();
-        paths.sort_by_key(|entry| entry.file_name());
-        for entry in paths {
-            if let Some(file_name) = entry.file_name().to_str() {
-                if cli.all || !file_name.starts_with(".") {
-                    if cli.long {
-                        let utc_offset = UtcOffset::current_local_offset().unwrap();
-                        if let Ok(metadata) = entry.metadata() {
-                            let mut parsed_modified = Parsed::new();
-                            parsed_modified = parsed_modified
-                                .with_unix_timestamp_nanos(
-                                    metadata
-                                        .modified()
-                                        .unwrap()
-                                        .duration_since(UNIX_EPOCH)
-                                        .unwrap()
-                                        .as_nanos()
-                                        .try_into()
-                                        .unwrap(),
-                                )
-                                .unwrap();
-                            println!(
-                                "{}\t{}\t{}\t{}\t{}",
-                                metadata.mode(),
-                                metadata.uid(),
-                                metadata.gid(),
-                                OffsetDateTime::try_from(parsed_modified)
-                                    .unwrap()
-                                    .to_offset(utc_offset)
-                                    .format(format_description!(
-                                        "[month repr:short] [day] [hour]:[minute]"
-                                    ))
-                                    .unwrap(),
-                                file_name
-                            );
-                        }
-                    } else {
-                        print!("{}  ", entry.file_name().to_string_lossy());
-                    }
-                }
-            }
-        }
-        if !cli.long {
-            println!();
+impl<'a> LSFile<'a> {
+    fn new(path: PathBuf, cli: &'a Cli) -> Self {
+        LSFile {
+            path,
+            cli,
+            metadata: None,
         }
     }
 
-    // println!("ls {}", path.to_path_buf().display())
+    fn load_metadata(&mut self) {
+        self.metadata = self.path.metadata().ok()
+    }
+
+    fn file_name(&self) -> Option<&std::ffi::OsStr> {
+        self.path.file_name()
+    }
+
+    fn mode(&self) -> Option<u32> {
+        match &self.metadata {
+            None => None,
+            Some(metadata) => Some(metadata.mode()),
+        }
+    }
+    fn uid(&self) -> Option<u32> {
+        match &self.metadata {
+            None => None,
+            Some(metadata) => Some(metadata.uid()),
+        }
+    }
+    fn gid(&self) -> Option<u32> {
+        match &self.metadata {
+            None => None,
+            Some(metadata) => Some(metadata.gid()),
+        }
+    }
+
+    fn size(&self) -> Option<u64> {
+        match &self.metadata {
+            None => None,
+            Some(metadata) => Some(metadata.size()),
+        }
+    }
+
+    fn modified(&self) -> Option<OffsetDateTime> {
+        match &self.metadata {
+            None => None,
+            Some(metadata) => {
+                let mut parsed_modified = Parsed::new();
+                parsed_modified = parsed_modified
+                    .with_unix_timestamp_nanos(
+                        metadata
+                            .modified()
+                            .unwrap()
+                            .duration_since(UNIX_EPOCH)
+                            .unwrap()
+                            .as_nanos()
+                            .try_into()
+                            .unwrap(),
+                    )
+                    .unwrap();
+                Some(
+                    OffsetDateTime::try_from(parsed_modified)
+                        .unwrap()
+                        .to_offset(*UTC_OFFSET),
+                )
+            }
+        }
+    }
+}
+
+impl<'a> Display for LSFile<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.path.file_name() {
+            None => Ok(()),
+            Some(file_name) => {
+                if self.cli.long {
+                    writeln!(
+                        f,
+                        "{}\t{}\t{}\t{}\t{}\t{}",
+                        self.mode().unwrap(),
+                        self.uid().unwrap(),
+                        self.gid().unwrap(),
+                        self.size().unwrap(),
+                        self.modified()
+                            .unwrap()
+                            .format(format_description!(
+                                "[month repr:short] [day padding:zero] [hour]:[minute]"
+                            ))
+                            .unwrap(),
+                        file_name.to_string_lossy()
+                    )
+                } else {
+                    write!(f, "{}  ", file_name.to_string_lossy())
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let mut paths = cli
+        .path
+        .clone()
+        .unwrap_or(vec![Path::new(".").to_path_buf()])
+        .into_iter()
+        .flat_map(|p| {
+            if p.is_dir() {
+                let paths = p
+                    .read_dir()
+                    .expect("Could not read dir")
+                    .filter_map(|entry| entry.ok())
+                    .map(|entry| entry.path())
+                    .collect::<Vec<PathBuf>>();
+                return paths;
+            }
+            return vec![p];
+        })
+        .filter(|path| {
+            return cli.all
+                || !path
+                    .file_name()
+                    .is_some_and(|n| n.as_bytes().starts_with(b"."));
+        })
+        .map(|p| LSFile::new(p, &cli))
+        .collect::<Vec<LSFile>>();
+    paths.sort_unstable_by_key(|entry| entry.file_name().unwrap().to_os_string());
+    for mut path in paths {
+        path.load_metadata();
+        print!("{}", path);
+    }
+    if !cli.long {
+        println!("")
+    }
 }
